abad07203f16bb979178fc373575904eb2834eb3
noahkise <noah@kisefamily.com>
2024-01-25 11:58:13 -0800

    add styling, lunch time
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- BeatBox/Controllers/ArtistsController.cs
+++ BeatBox/Controllers/ArtistsController.cs
@@ -4,8 +4,26 @@ using System.Collections.Generic;
 
 namespace BeatBox.Controllers
 {
-  public class ArtistController : Controller
+  public class ArtistsController : Controller
   {
+    [HttpGet("/artists")]
+    public ActionResult Index()
+    {
+      List<Artist> allArtists = Artist.GetAll();
+      return View(allArtists);
+    }
 
+    [HttpGet("/artists/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/artists")]
+    public ActionResult Create(string artistName)
+    {
+      Artist newArtist = new(artistName);
+      return RedirectToAction("Index");
+    }
   }
 }
